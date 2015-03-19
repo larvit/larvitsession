7d4bd5a692137975330252fa448d7f4badaffc6b
lillem4n <lilleman@larvit.se>
2015-03-19 18:49:16 +0100

    Updated database schema

--- larvitsession.js
+++ larvitsession.js
@@ -7,12 +7,12 @@ var log         = require('winston'),
 
 function createDb(callback) {
 	var sql = 'CREATE TABLE IF NOT EXISTS `sessions` (' +
-	          '  `uuid` char(36) COLLATE utf8_unicode_ci NOT NULL,' +
-	          '  `json` varchar(16384) COLLATE utf8_unicode_ci NOT NULL,' +
+	          '  `uuid` char(36) COLLATE ascii_general_ci NOT NULL,' +
+	          '  `json` varchar(15000) COLLATE utf8mb4_unicode_ci NOT NULL,' +
 	          '  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,' +
 	          '  PRIMARY KEY (`uuid`),' +
 	          '  KEY `updated` (`updated`)' +
-	          ') ENGINE=MEMORY DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;';
+	          ') ENGINE=MEMORY DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;';
 
 	db.query(sql, function(err) {
 		if (err) {
